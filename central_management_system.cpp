1005267dea552db3f00e4d7094881325b31f7e76
192373046 <divyamalluri796@gmail.com>
2025-11-08 09:10:59 +0530

    Add files via upload

---
+++ central_management_system.cpp
@@ -0,0 +1,28 @@
+#include "central_management_system.h"
+
+// Constructors/Destructors
+
+
+central_management_system::central_management_system()
+{
+  initAttributes();
+}
+
+central_management_system::~central_management_system()
+{
+}
+
+// Methods
+
+
+// Accessor methods
+
+
+
+// Other methods
+
+
+void central_management_system::initAttributes()
+{
+}
+
