7ecf1f3c7d3b4c747c79c78448a2fe0369427942
192373046 <divyamalluri796@gmail.com>
2025-11-08 09:07:20 +0530

    Add files via upload

---
+++ admin.cpp
@@ -0,0 +1,28 @@
+#include "admin.h"
+
+// Constructors/Destructors
+
+
+admin::admin()
+{
+  initAttributes();
+}
+
+admin::~admin()
+{
+}
+
+// Methods
+
+
+// Accessor methods
+
+
+
+// Other methods
+
+
+void admin::initAttributes()
+{
+}
+
